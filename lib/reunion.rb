49c72442844ba81aac67e03f7df142d9d552d70f
abarnes26 <abarnes26@yahoo.com>
2017-11-01 10:14:15 -0600

    building reunion functionality

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,12 +1,23 @@
 class Reunion
-  attr_reader :location, :activities
+  attr_reader :location, :activities, :total_activity_cost
 
   def initialize(location)
     @location = location
     @activities = []
+    @total_activity_cost = []
   end
 
   def add_activities(activity)
     @activities << activity
   end
+
+  def add_up_activity_cost
+    @activities.map do |activity|
+      @total_activity_cost << activity.total_cost
+    end
+    @total_activity_cost.sum
+  end
+
+
+  
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -27,7 +27,41 @@ class ReunionTest < Minitest::Test
      activity = Activity.new("Volleyball")
      reunion.add_activities(activity)
 
-     assert_equal "Volleyball", reunion.activities.first.activity_name
+     assert_equal 1, reunion.activities.count
+     assert_equal "Volleyball", reunion.activities[0].activity_name
    end
 
+   def test_it_can_take_multiple_activities
+     reunion = Reunion.new("Central Park")
+     activity1 = Activity.new("Volleyball")
+     activity2 = Activity.new("Tennis")
+     reunion.add_activities(activity1)
+     reunion.add_activities(activity2)
+
+     assert_equal 2, reunion.activities.count
+     assert_equal "Volleyball", reunion.activities[0].activity_name
+     assert_equal "Tennis", reunion.activities[1].activity_name
+   end
+
+   def test_it_can_total_costs_from_multiple_activites
+     reunion = Reunion.new("Central Park")
+     activity1 = Activity.new("Volleyball")
+     activity1.add_participant({"name" => "Chris",
+                                "paid" => 14})
+     activity1.add_participant({"name" => "Harrison",
+                                "paid" => 15})
+     activity1.add_participant({"name" => "Katherine",
+                                "paid" => 12})
+     activity1.add_participant({"name" => "Joan",
+                                "paid" => 19})
+     activity2 = Activity.new("Tennis")
+     activity2.add_participant({"name" => "Chris",
+                                "paid" => 14})
+     activity2.add_participant({"name" => "Harrison",
+                                "paid" => 16})
+     reunion.add_activities(activity1)
+     reunion.add_activities(activity2)
+
+     assert_equal 90, reunion.add_up_activity_cost
+   end
 end
