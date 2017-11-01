12a82331c0f1bd8293bc77bd21bff1086213878c
abarnes26 <abarnes26@yahoo.com>
2017-11-01 09:35:15 -0600

    through iteration 1, lot of second guessing

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
 class Activity
+  attr_reader :activity_name, :participants
+
+  def initialize(activity_name)
+    @activity_name = activity_name
+    @participants = []
+  end
+
+  def add_participant(participant)
+    @participants << participant
+  end
+
+  def total_cost
+    total_cost = []
+    @participants.map do |participant|
+      total_cost << participant["paid"]
+    end
+    total_cost
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,56 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_instantiates
+    assert_instance_of Activity, Activity.new("Tennis")
+  end
+
+  def test_the_activity_has_a_name
+    activity = Activity.new("Tennis")
+
+    assert_equal "Tennis", activity.activity_name
+  end
+
+  def test_the_activity_has_participants
+    activity = Activity.new("Tennis")
+
+    assert_equal [], activity.participants
+  end
+
+  def test_you_can_add_participants_to_an_event
+    activity = Activity.new("Tennis")
+    activity.add_participant({"name" => "Chris",
+                              "paid" => 14})
+
+    assert_equal 1,       activity.participants.count
+    assert_equal "Chris", activity.participants[0]["name"]
+    assert_equal 14,      activity.participants[0]["paid"]
+  end
+
+  def test_an_activity_can_have_more_than_one_participant
+    activity = Activity.new("Tennis")
+    activity.add_participant({"name" => "Chris",
+                              "paid" => 14})
+    activity.add_participant({"name" => "Harrison",
+                              "paid" => 16})
+
+
+    assert_equal 2,          activity.participants.count
+    assert_equal "Harrison", activity.participants[1]["name"]
+    assert_equal 16,         activity.participants[1]["paid"]
+  end
+
+  def test_an_activity_has_a_total_cost
+    activity = Activity.new("Tennis")
+    activity.add_participant({"name" => "Chris",
+                              "paid" => 14})
+    activity.add_participant({"name" => "Harrison",
+                              "paid" => 16})
+
+    assert_equal 30, activity.total_cost
+  end
+
+
+
 end
