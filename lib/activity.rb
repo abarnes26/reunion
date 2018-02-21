e98e356ad6d55dc5aa405ea2be712a9b2883acc1
agpiermarini <piermarini.andrew@gmail.com>
2018-02-21 09:30:12 -0700

    intitial commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,12 @@
 class Activity
+  attr_reader :participants
+  def initialize(name)
+    @name = name
+    @participants = []
+    @cost = 0
+  end
+
+  def add_participant(name, paid)
+    @participants << {name: name, paid: paid}
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,25 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("hiking")
+    assert_instance_of Activity, activity
+  end
+
+  def test_participants_hash
+    activity = Activity.new("hiking")
+    assert_instance_of Array, activity.participants
+  end
+
+  def test_can_add_participants_to_hash
+    activity = Activity.new("hiking")
+    assert_nil activity.participants.first
+
+    activity.add_participant("Andrew", 5)
+    assert_equal "Andrew", activity.participants.first[:name]
+    assert_equal 5, activity.participants.first[:paid]
+  end
+
+
+
 end
